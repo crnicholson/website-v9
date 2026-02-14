097a8ce302394f464c6944c47d4679ae0391dc77
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2026-02-14 12:47:26 -0500

    i put my name on the website

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 import Slider from '@/components/Slider'
 
 export default function Home() {
@@ -6,6 +8,16 @@ export default function Home() {
       <div className="w-full h-screen flex items-center justify-center relative">
         <Slider />
         <div className="pointer-events-none absolute inset-0 z-10 shadow-[inset_80px_0_300px_rgba(255,255,255,1),inset_-80px_0_300px_rgba(255,255,255,1)]" />
+
+        <div className="pointer-events-none absolute inset-0 z-20 h-screen w-full flex flex-col items-center justify-between">
+          <div className="flex flex-row items-center justify-between h-fit w-full px-8 py-6">
+            <h1 className="">charlie</h1>
+          </div>
+          <div className="flex flex-row items-center justify-between h-fit w-full px-8 py-6">
+            <h1 className=""></h1>
+            <Link href="https://www.instagram.com/charliennnicholson/" className="hover:text-shadow-none pointer-events-auto">@charliennnicholson</Link>
+          </div>
+        </div>
 
         {/* <figure className="w-50 h-50 pointer-events-auto perspective-[1143px] -rotate-y-50 skew-y-20 transform">
           <div className="absolute inset-0 rounded-inherit">
